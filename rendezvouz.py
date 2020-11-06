d54997f7f26d9c137221050eb5d94e8d6f7a4b75
austin5627 <austinsharris@gmail.com>
2020-11-06 17:22:28 -0600

    Reworked rendezvouz.py so that it actually works Moved everything into functions

--- rendezvouz.py
+++ rendezvouz.py
@@ -11,7 +11,105 @@ def match_inclination(vessel, target):
     node_ut = vessel.orbit.ut_at_true_anomaly(an_ta)
     node_dv = 2 * vessel.orbit.speed * math.sin(vessel.orbit.relative_inclination(target.orbit) / 2)
     node = vessel.control.add_node(node_ut, 0, -node_dv, 0)
-    return node
+    deltaV.execute_node(vessel, node, conn)
+
+
+def get_close_approach(vessel, target):
+    print("Create Close Approach")
+
+    # Get Angle Between target and vessel
+    # TODO: Only works if vessel is behind target
+    v_pos = vessel.position(vessel.orbit.body.non_rotating_reference_frame)
+    t_pos = target.position(target.orbit.body.non_rotating_reference_frame)
+    v_pos = (v_pos[0], v_pos[2])  # Ignore Altitude
+    t_pos = (t_pos[0], t_pos[2])
+    unit_v = v_pos / numpy.linalg.norm(v_pos)  # Convert to unit vectors
+    unit_t = t_pos / numpy.linalg.norm(t_pos)
+
+    dot_product = numpy.dot(unit_v, unit_t)
+    angle = numpy.arccos(numpy.clip(dot_product, -1.0, 1.0))
+    print(angle)
+
+    orbit = vessel.orbit
+    period = orbit.period
+    mu = orbit.body.gravitational_parameter
+
+    # Find the time between target and vessel
+    time_from_target = period * ((math.tau - angle) / math.tau)
+    # Change orbit to be time_from_target longer
+    new_period = period + time_from_target
+    new_semi_major_axis = math.pow((new_period / math.tau) ** 2 * mu, (1. / 3.))
+    new_apoapsis = new_semi_major_axis * 2 - orbit.periapsis
+
+    # delta v required for orbit change
+    r = vessel.orbit.periapsis
+    a1 = vessel.orbit.semi_major_axis
+    a2 = new_semi_major_axis
+    v1 = math.sqrt(mu * ((2.0 / r) - (1.0 / a1)))
+    v2 = math.sqrt(mu * ((2.0 / r) - (1.0 / a2)))
+    delta_v = v2 - v1
+
+    # execute orbit change
+    node_ut = conn.space_center.ut + 30
+    node = vessel.control.add_node(node_ut, prograde=delta_v)
+    deltaV.execute_node(vessel, node, conn)
+
+
+def match_velocities(vessel, target, conn, max_throttle=1):
+    print("Match Velocities")
+
+    print("Point Retrograde")
+    vessel.auto_pilot.reference_frame = target.orbital_reference_frame
+    v = vessel.flight(target.orbital_reference_frame).velocity
+    n_v = (-v[0], -v[1], -v[2])
+    vessel.auto_pilot.target_direction = n_v
+    vessel.auto_pilot.engage()
+    vessel.auto_pilot.wait()
+
+    print("Throttle up")
+    target_sp = conn.add_stream(getattr, vessel.flight(target.orbital_reference_frame), "speed")
+    vessel.control.throttle = max_throttle
+    time.sleep(.1)
+    last_sp = target_sp()
+    while target_sp() <= last_sp:
+        last_sp = target_sp()
+        time.sleep(0.01)
+    vessel.control.throttle = 0
+
+
+def fine_tune_closest_approach(vessel, target, conn):
+    target_position = conn.add_stream(target.position, vessel.orbital_reference_frame)
+    target_sp = conn.add_stream(getattr, vessel.flight(target.orbital_reference_frame), "speed")
+    sensitivities = [[50, 1, 400], [30, .5, 200], [5, .1, 50]]
+    while numpy.linalg.norm(target_position()) > 50:
+        vessel.auto_pilot.reference_frame = vessel.orbital_reference_frame
+        vessel.auto_pilot.target_direction = target_position()
+        vessel.auto_pilot.engage()
+        vessel.auto_pilot.wait()
+        dist = numpy.linalg.norm(target_position())
+        if dist > 1000:
+            x = 0
+        elif dist > 500:
+            x = 1
+        else:
+            x = 2
+        vessel.control.throttle = sensitivities[x][1]
+        while target_sp() < sensitivities[x][0]:
+            pass
+        vessel.control.throttle = 0
+        vessel.auto_pilot.reference_frame = target.orbital_reference_frame
+        v = vessel.flight(target.orbital_reference_frame).velocity
+        n_v = (-v[0], -v[1], -v[2])
+        vessel.auto_pilot.target_direction = n_v
+        vessel.auto_pilot.engage()
+        vessel.auto_pilot.wait()
+        last_dist = numpy.linalg.norm(target_position())
+        time.sleep(0.1)
+        while sensitivities[x][2] < numpy.linalg.norm(target_position()) < last_dist:
+            last_dist = numpy.linalg.norm(target_position())
+            print(last_dist)
+            time.sleep(0.1)
+        match_velocities(vessel, target, conn, x[1])
 
 
 conn = krpc.connect(name='rendezvous')
@@ -23,60 +121,17 @@ if target is None:
     exit(-2)
 
 if vessel.orbit.relative_inclination(target.orbit) > 0.1:
-    node = match_inclination(vessel, target)
-    deltaV.execute_node(vessel, node, conn)
+    match_inclination(vessel, target)
 
 # Rendezvous
-
 print("Rendezvous")
-v_pos = vessel.position(vessel.orbit.body.non_rotating_reference_frame)
-t_pos = target.position(target.orbit.body.non_rotating_reference_frame)
-v_pos = (v_pos[0], v_pos[2])
-t_pos = (t_pos[0], t_pos[2])
-unit_v = v_pos / numpy.linalg.norm(v_pos)
-unit_t = t_pos / numpy.linalg.norm(t_pos)
 
-dot_product = numpy.dot(unit_v, unit_t)
-angle = numpy.arccos(numpy.clip(dot_product, -1.0, 1.0))
+get_close_approach(vessel, target)
 
-orbit = vessel.orbit
-period = orbit.period
-mu = orbit.body.gravitational_parameter
-
-time_from_target = period * ((math.tau - angle) / math.tau)
-new_period = period + time_from_target
-new_semi_major_axis = math.pow((new_period / math.tau) ** 2 * mu, (1. / 3.))
-new_apoapsis = new_semi_major_axis * 2 - orbit.periapsis
-
-r = vessel.orbit.periapsis
-a1 = vessel.orbit.semi_major_axis
-a2 = new_semi_major_axis
-v1 = math.sqrt(mu * ((2.0 / r) - (1.0 / a1)))
-v2 = math.sqrt(mu * ((2.0 / r) - (1.0 / a2)))
-delta_v = v2 - v1
-
-node_ut = conn.space_center.ut + 30
-node = vessel.control.add_node(node_ut, prograde=delta_v)
-deltaV.execute_node(vessel, node, conn)
-
-# Match Velocities
-print("Match Velocities")
-ca_time = orbit.time_of_closest_approach(target.orbit)
+ca_time = vessel.orbit.time_of_closest_approach(target.orbit)
 conn.space_center.warp_to(ca_time - 10)
 
-print("Point Retrograde")
-vessel.auto_pilot.reference_frame = target.orbital_reference_frame
-v = vessel.flight(target.orbital_reference_frame).velocity
-n_v = (-v[0], -v[1], -v[2])
-vessel.auto_pilot.target_direction = n_v
-vessel.auto_pilot.engage()
-vessel.auto_pilot.wait()
+match_velocities(vessel, target, conn)
 
-print("Throttle up")
-target_sp = conn.add_stream(getattr, vessel.flight(target.orbital_reference_frame), "speed")
-vessel.control.throttle = 1
-last_sp = target_sp()
-while target_sp() <= last_sp:
-    last_sp = target_sp()
-    print(target_sp())
-vessel.control.throttle = 0
+fine_tune_closest_approach(vessel, target, conn)
+
